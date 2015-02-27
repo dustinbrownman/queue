26cb526d9e54e952225e8a647fc0ffca5b470816
Dustin Brown <dbrown9@gmail.com>
2015-02-26 20:55:16 -0800

    simplify filtering of open tickets

--- app/controllers/queue.js
+++ app/controllers/queue.js
@@ -2,20 +2,11 @@ import Ember from 'ember';
 
 var QueueController = Ember.ArrayController.extend({
   itemController: 'ticket',
-  tickets: function() {
-    var openTickets = this.get('model').filter(function(ticket) {
+  openTickets: function() {
+    return this.get('model').filter(function(ticket) {
       return ticket.get('open');
     });
 
-    var orderedTickets = openTickets.sort(function(a, b) {
-      if (a.get('createdAt') > b.get('createdAt')) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-
-    return orderedTickets;
   }.property('model.@each.open')
 });
 
