1823c3d083cd0701b9acbd75165a834d615446ba
Dustin Brown <dbrown9@gmail.com>
2015-05-06 16:34:20 -0700

    update ember version to 1.11.1
    This also allowed liquid-fire to update as well.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
+import config from '../config/environment';
 import Firebase from 'firebase';
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
-  firebase: new Firebase('https://ember-queue.firebaseio.com')
+  firebase: new Firebase(config.firebase)
 });
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'queue',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://ember-queue.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
